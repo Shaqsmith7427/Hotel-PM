8c670fbd6f33a4c1f0e4f3d18e37f07d2e424065
Kaz <shaqsmith7427@hotmail.com>
2019-09-24 10:37:14 -0400

    Added flashlight and battery test objects

---
+++ Hotel_PM/Assets/Scripts/Character_Controller.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour
+{
+    [SerializeField] public float p_speed = 10.0f;
+    [SerializeField] public float t_speed = 3.0f;
+    [SerializeField] public float spr_speed = 5.0f;
+
+     Rigidbody p_Rigidbody;
+     public Flashlight flash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p_Rigidbody = GetComponent<Rigidbody>();
+        //flash = GetComponent<Flashlight>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float vert = Input.GetAxis("Vertical");
+
+        vert *= Time.deltaTime;
+        transform.Translate(0, 0, vert * p_speed);
+
+        bool isTurningLeft = Input.GetKey(KeyCode.A);
+        bool isTurningRight = Input.GetKey(KeyCode.D);
+        if (isTurningLeft && !isTurningRight)
+        {
+            transform.Rotate(Vector3.down * t_speed);
+        }
+
+        else if (!isTurningLeft && isTurningRight)
+        {
+            transform.Rotate(Vector3.up * t_speed);
+        }
+
+       /* bool isRunning = Input.GetKeyDown("Space");
+        bool isNotRunning = Input.GetKeyDown("Space");
+
+
+        if (isRunning)
+        {
+            transform.Translate(0, 0, vert * spr_speed);
+        }
+        else if (isNotRunning)
+                {
+                    transform.Translate(0, 0, vert * p_speed);
+                }*/
+    }
+
+    private void OnTriggerEnter(Collider other)
+    { Debug.Log("Collided");
+        if (other.gameObject.CompareTag("Battery"))
+        {
+            other.gameObject.SetActive(false);
+            flash.flashlight.intensity += 10;
+        }
+       
+    }
+}
+
---
+++ Hotel_PM/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+
+    public Light flashlight;
+    public bool on;
+    //public float timer = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flashlight.enabled = false;
+        on = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (on == true)
+            {
+                on = false;
+                flashlight.enabled = false;
+            }
+            else //(on == false)
+            {
+                on = true;
+                flashlight.enabled = true;
+            }
+ 
+        }
+
+           if (on == true)
+            {
+               // flashlight.enabled = true;
+                if (flashlight.intensity > 0)
+                {
+                    flashlight.intensity -= 5.0f * Time.deltaTime;
+                }
+                else
+                {
+                     flashlight.intensity = 0;
+                     on = false;
+                     flashlight.enabled = false;
+            }
+           }        
+
+    }
+}
+       
+
+
+
+
