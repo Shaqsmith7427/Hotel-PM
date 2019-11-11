1a66054b31be8d516c67d97146a2816b670ab2b0
Jake <jhaggar2@uncc.edu>
2019-11-11 18:01:25 -0500

    Fan-Blades rotate
    Fan-Blades rotate. Speed is set to a random value between 45 & 180.

--- Hotel_PM/Assets/Scripts/Light_Controller.cs
+++ Hotel_PM/Assets/Scripts/Light_Controller.cs
@@ -7,6 +7,7 @@ public class Light_Controller : MonoBehaviour
     #region Variables
     [SerializeField] Light light;
 
+    [SerializeField] private float rotationSpeed;
     private bool isLightOn;
     #endregion
 
@@ -16,12 +17,16 @@ public class Light_Controller : MonoBehaviour
     {
         //Lights are off by default (not including lights in the hallway)
         isLightOn = false;
+
+        //Sets fan-blade rotation to a random value within the range
+        rotationSpeed = Random.Range(45f, 180f);
     }
 
     // Update is called once per frame
     void Update()
     {
         handleLight();
+        RotateFanBlades();
     }
     #endregion
 
@@ -41,6 +46,11 @@ public class Light_Controller : MonoBehaviour
     public void setLight()
     {
         isLightOn = !isLightOn;
+    }
+
+    void RotateFanBlades()
+    {
+        transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
     }
     #endregion
 }
