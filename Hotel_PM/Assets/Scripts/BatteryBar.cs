db2e6cfda417d0c2431a9fb37223eb9f955579b4
Kaz <shaqsmith7427@hotmail.com>
2019-11-25 21:51:42 -0500

    Ending Scene and Music
    Added End scene and music, changed the image for the start screen.

---
+++ Hotel_PM/Assets/Scripts/BatteryBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryBar : MonoBehaviour
+{
+    private Transform bar;
+    public Light flashlight;
+
+
+    private void Start()
+    {
+        Transform bar = transform.Find("Bar");
+        bar.localScale = new Vector3(flashlight.intensity, 1f);
+
+    }
+
+   
+}
